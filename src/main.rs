f0acfd548b7c92215020db2b9ce34aaa5e79509b
kalium <kalium@kalium.xyz>
2019-08-03 18:56:31 +0900

    feat(vga_buffer) add print and println macros

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ mod vga_buffer;
 
 // function to be called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("hello!").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", numbers are {} and {}", 42, 1.0/3.0).unwrap();
+    println!("hello world{}", "!");
 
     loop {}
 }
