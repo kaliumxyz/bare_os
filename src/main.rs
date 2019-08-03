3bce98e07dc15be36c2fe1a95c3df6924b03ac95
kalium <kalium@kalium.xyz>
2019-08-03 17:49:41 +0900

    feat(vga_buffer) add a vga buffer interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 // function to be called on panic
 #[panic_handler]
@@ -12,14 +13,15 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::print_something();
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
 
     loop {}
 }
